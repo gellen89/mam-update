0356177e5b74376f6ec53d7ac6ed4418ff3d666c
gellen89 <186510403+gellen89@users.noreply.github.com>
2024-10-27 16:27:36 -0700

    updates code to be more streamlined

--- main.go
+++ main.go
@@ -17,228 +17,88 @@ import (
 	"github.com/gellen89/mam-update-go/internal/appdir"
 )
 
-func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
-
-	appdirs, err := appdir.GetAppDirs("mamdynupdate")
-	if err != nil {
-		panic(err)
-	}
-	err = appdirs.EnsureDirs()
-	if err != nil {
-		panic(err)
-	}
-
-	mamId := os.Getenv("MAM_ID")
-	cookieFileExists, err := hasCookieFile(appdirs.Data, "MAM.cookie")
-	if err != nil {
-		panic(err)
-	}
-	if !cookieFileExists && mamId == "" {
-		panic("no cookie file found, must provided MAM_ID for initialization")
-	}
-
-	currentIp, err := getCurrentIpAddress()
-	if err != nil {
-		panic(err)
-	}
-
-	if !cookieFileExists {
-		resp, err := setIpAddress(&http.Client{}, logger)
-		if err != nil {
-			panic(err)
-		}
-		if !resp.Resp.Success {
-			logger.Error(fmt.Sprintf("IP Address Update did not complete successfully. Message: %q", resp.Resp.Msg))
-			return
-		}
-		logger.Debug(fmt.Sprintf("IP Address update completed successfully. Message: %q", resp.Resp.Msg))
-
-		logger.Debug("writing cookies...")
-		file, err := os.Create(filepath.Join(appdirs.Data, "MAM.cookie"))
-		if err != nil {
-			panic(err)
-		}
-		encoder := gob.NewEncoder(file)
-		err = encoder.Encode(resp.Cookies)
-		if err != nil {
-			panic(err)
-		}
-		logger.Debug("successfully wrote cookies to file")
-		err = os.WriteFile("last_run_time", []byte(time.Now().Format(time.RFC3339)), 0666)
-		if err != nil {
-			panic(err)
-		}
-		logger.Debug("IP Address update success")
-		return
-	}
-
-	oldIpAddress, oldIpExists, err := getOldIpAddress(appdirs.Data, "MAM.ip")
-	if err != nil {
-		panic(err)
-	}
-
-	if oldIpExists && oldIpAddress == currentIp {
-		logger.Info("IP Addresses have not changed, skipping...")
-		return
-	}
-	logger.Info("IP Addresses have changed since previous run...")
-
-	currentTime := time.Now()
-	lastRunTime, hasLastRun, err := getLastRunTime(appdirs.Data, "last_run_time")
-	if err != nil {
-		panic(err)
-	}
-	if hasLastRun {
-		duration := currentTime.Sub(*lastRunTime)
-		if duration <= time.Hour {
-			logger.Info("Last run was within the last hour, skipping...")
-			return
-		}
-	}
-
-	file, err := os.Open(filepath.Join(appdirs.Data, "MAM.cookie"))
-	if err != nil {
-		panic(err)
-	}
-	var cookies []*http.Cookie
-	decoder := gob.NewDecoder(file)
-	if err = decoder.Decode(&cookies); err != nil {
-		panic(err)
-	}
-	jar, err := cookiejar.New(&cookiejar.Options{})
-	if err != nil {
-		panic(err)
-	}
-	inputUrl, err := url.Parse(dynSeedBoxUrl)
-	if err != nil {
-		panic(err)
-	}
-	jar.SetCookies(inputUrl, cookies)
-	client := &http.Client{Jar: jar}
-	resp, err := setIpAddress(client, logger)
-	if err != nil {
-		panic(err)
-	}
-
-	if !resp.Resp.Success {
-		logger.Error(fmt.Sprintf("IP Address Update did not complete successfully. Message: %q", resp.Resp.Msg))
-		return
-	}
-	logger.Debug(fmt.Sprintf("IP Address update completed successfully. Message: %q", resp.Resp.Msg))
-
-	logger.Debug("writing cookies...")
-	file, err = os.Create(filepath.Join(appdirs.Data, "MAM.cookie"))
-	if err != nil {
-		panic(err)
-	}
-	encoder := gob.NewEncoder(file)
-	err = encoder.Encode(resp.Cookies)
-	if err != nil {
-		panic(err)
-	}
-	logger.Debug("successfully wrote cookies to file")
-	err = os.WriteFile("last_run_time", []byte(time.Now().Format(time.RFC3339)), 0666)
-	if err != nil {
-		panic(err)
-	}
-	logger.Debug("IP Address update success")
+type Config struct {
+	DataDir     string
+	CookiePath  string
+	IpPath      string
+	LastRunPath string
+	Logger      *slog.Logger
 }
 
-const (
-	dynSeedBoxUrl = "https://t.myanonamouse.net/json/dynamicSeedbox.php"
-)
+type MamUpdater struct {
+	config     Config
+	httpClient *http.Client
+	logger     *slog.Logger
+}
 
 type dynamicSeedboxResponse struct {
 	Success bool   `json:"Success"`
 	Msg     string `json:"msg"`
 }
 
-type setIpAddressResponse struct {
-	Resp    *dynamicSeedboxResponse
-	Code    int
-	Cookies []*http.Cookie
-}
+const (
+	dynSeedBoxUrl = "https://t.myanonamouse.net/json/dynamicSeedbox.php"
+	minWaitPeriod = time.Hour
+)
 
-func setIpAddress(client *http.Client, logger *slog.Logger) (*setIpAddressResponse, error) {
-	inputUrl, err := url.Parse(dynSeedBoxUrl)
+func NewMamUpdater(config Config) (*MamUpdater, error) {
+	jar, err := cookiejar.New(&cookiejar.Options{})
 	if err != nil {
-		return nil, err
-	}
-
-	resp, err := client.Do(&http.Request{
-		Method: "GET",
-		URL:    inputUrl,
-	})
-	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to create cookie jar: %w", err)
 	}
 
-	defer resp.Body.Close()
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	logger.Debug(string(body))
-	if resp.StatusCode != 403 {
-		return nil, fmt.Errorf("unable to update ip address %s: %s", resp.Status, string(body))
-	}
-	var jsonResp dynamicSeedboxResponse
-	err = json.Unmarshal(body, &jsonResp)
-	if err != nil {
-		return nil, err
-	}
-
-	return &setIpAddressResponse{
-		Resp:    &jsonResp,
-		Code:    resp.StatusCode,
-		Cookies: resp.Cookies(),
+	return &MamUpdater{
+		config:     config,
+		httpClient: &http.Client{Jar: jar},
+		logger:     config.Logger,
 	}, nil
 }
 
-func hasCookieFile(dataDir string, fileName string) (bool, error) {
-	_, err := os.Stat(filepath.Join(dataDir, fileName))
-	if err != nil && !os.IsNotExist(err) {
-		return false, err
-	} else if err != nil && os.IsNotExist(err) {
-		return false, nil
-	}
-	return true, nil
-}
-
-func getLastRunTime(dataDir string, fileName string) (*time.Time, bool, error) {
-	path := filepath.Join(dataDir, fileName)
-
-	bits, err := os.ReadFile(path)
-	if err != nil && !os.IsNotExist(err) {
-		return nil, false, err
-	} else if err != nil && os.IsNotExist(err) {
-		return nil, false, nil
-	}
-	output, err := time.Parse(time.RFC3339, string(bits))
+func (m *MamUpdater) Run() error {
+	// Check for cookie file or MAM_ID
+	hasCookie, err := m.hasCookieFile()
 	if err != nil {
-		return nil, false, err
+		return fmt.Errorf("failed to check cookie file: %w", err)
 	}
-	return &output, true, nil
+
+	mamID := os.Getenv("MAM_ID")
+	if !hasCookie && mamID == "" {
+		return fmt.Errorf("no cookie file found and MAM_ID not provided")
+	}
+
+	// Get current IP
+	currentIP, err := m.getCurrentIP()
+	if err != nil {
+		return fmt.Errorf("failed to get current IP: %w", err)
+	}
+
+	// First run without cookie
+	if !hasCookie {
+		return m.handleFirstRun(mamID)
+	}
+
+	// Check if IP has changed
+	if changed, err := m.hasIPChanged(currentIP); err != nil {
+		return fmt.Errorf("failed to check IP change: %w", err)
+	} else if !changed {
+		m.logger.Info("IP address hasn't changed, skipping update")
+		return nil
+	}
+
+	// Check last run time
+	if shouldSkip, err := m.shouldSkipUpdate(); err != nil {
+		return fmt.Errorf("failed to check last run time: %w", err)
+	} else if shouldSkip {
+		m.logger.Info("Last run was too recent, skipping update")
+		return nil
+	}
+
+	// Update IP
+	return m.updateIP(nil) // nil means use existing cookies
 }
 
-func getOldIpAddress(dataDir string, fileName string) (string, bool, error) {
-	path := filepath.Join(dataDir, fileName)
-
-	bits, err := os.ReadFile(path)
-	if err != nil && !os.IsNotExist(err) {
-		return "", false, err
-	} else if err != nil && os.IsNotExist(err) {
-		return "", false, nil
-	}
-	return string(bits), true, nil
-}
-
-func getCurrentIpAddress() (string, error) {
-	client := &http.Client{
-		Timeout: 10 * time.Second,
-	}
-	resp, err := client.Get("https://ifconfig.io/ip")
+func (m *MamUpdater) getCurrentIP() (string, error) {
+	resp, err := http.Get("https://ifconfig.io/ip")
 	if err != nil {
 		return "", err
 	}
@@ -249,4 +109,175 @@ func getCurrentIpAddress() (string, error) {
 		return "", err
 	}
 	return strings.TrimSpace(string(ip)), nil
+}
+
+func (m *MamUpdater) hasIPChanged(currentIP string) (bool, error) {
+	oldIP, exists, err := m.readFile(m.config.IpPath)
+	if err != nil {
+		return false, err
+	}
+	return !exists || oldIP != currentIP, nil
+}
+
+func (m *MamUpdater) shouldSkipUpdate() (bool, error) {
+	lastRunStr, exists, err := m.readFile(m.config.LastRunPath)
+	if err != nil {
+		return false, err
+	}
+	if !exists {
+		return false, nil
+	}
+
+	lastRun, err := time.Parse(time.RFC3339, lastRunStr)
+	if err != nil {
+		return false, err
+	}
+
+	return time.Since(lastRun) < minWaitPeriod, nil
+}
+
+func (m *MamUpdater) updateIP(initialCookies []*http.Cookie) error {
+	targetURL, err := url.Parse(dynSeedBoxUrl)
+	if err != nil {
+		return fmt.Errorf("failed to parse URL: %w", err)
+	}
+
+	// If initial cookies provided (first run), use them
+	if initialCookies != nil {
+		m.httpClient.Jar.SetCookies(targetURL, initialCookies)
+	} else {
+		// Load existing cookies if no initial cookies provided
+		if err := m.loadCookies(); err != nil {
+			return fmt.Errorf("failed to load cookies: %w", err)
+		}
+	}
+
+	// Make request
+	resp, err := m.httpClient.Get(dynSeedBoxUrl)
+	if err != nil {
+		return fmt.Errorf("failed to make request: %w", err)
+	}
+	defer resp.Body.Close()
+
+	// Parse response
+	var result dynamicSeedboxResponse
+	if err := json.NewDecoder(resp.Body).Decode(&result); err != nil {
+		return fmt.Errorf("failed to decode response: %w", err)
+	}
+
+	if !result.Success {
+		return fmt.Errorf("IP update failed: %s", result.Msg)
+	}
+
+	// Save cookies and update timestamp
+	if err := m.saveCookies(resp.Cookies()); err != nil {
+		return fmt.Errorf("failed to save cookies: %w", err)
+	}
+
+	if err := m.writeFile(m.config.LastRunPath, time.Now().Format(time.RFC3339)); err != nil {
+		return fmt.Errorf("failed to update last run time: %w", err)
+	}
+
+	m.logger.Info("IP address updated successfully", "message", result.Msg)
+	return nil
+}
+
+func (m *MamUpdater) handleFirstRun(mamID string) error {
+	// Create initial cookie with MAM_ID
+	initialCookies := []*http.Cookie{
+		{
+			Name:  "mam_id",
+			Value: mamID,
+		},
+	}
+
+	if err := m.updateIP(initialCookies); err != nil {
+		return fmt.Errorf("first run failed: %w", err)
+	}
+	return nil
+}
+
+func (m *MamUpdater) hasCookieFile() (bool, error) {
+	_, err := os.Stat(m.config.CookiePath)
+	return !os.IsNotExist(err), nil
+}
+
+func (m *MamUpdater) loadCookies() error {
+	file, err := os.Open(m.config.CookiePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	var cookies []*http.Cookie
+	if err := gob.NewDecoder(file).Decode(&cookies); err != nil {
+		return err
+	}
+
+	u, err := url.Parse(dynSeedBoxUrl)
+	if err != nil {
+		return err
+	}
+
+	m.httpClient.Jar.SetCookies(u, cookies)
+	return nil
+}
+
+func (m *MamUpdater) saveCookies(cookies []*http.Cookie) error {
+	file, err := os.Create(m.config.CookiePath)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	return gob.NewEncoder(file).Encode(cookies)
+}
+
+func (m *MamUpdater) readFile(path string) (string, bool, error) {
+	data, err := os.ReadFile(path)
+	if os.IsNotExist(err) {
+		return "", false, nil
+	}
+	if err != nil {
+		return "", false, err
+	}
+	return string(data), true, nil
+}
+
+func (m *MamUpdater) writeFile(path string, content string) error {
+	return os.WriteFile(path, []byte(content), 0666)
+}
+
+func main() {
+	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
+
+	appdirs, err := appdir.GetAppDirs("mamdynupdate")
+	if err != nil {
+		logger.Error("Failed to get app directories", "error", err)
+		os.Exit(1)
+	}
+
+	if err := appdirs.EnsureDirs(); err != nil {
+		logger.Error("Failed to ensure directories exist", "error", err)
+		os.Exit(1)
+	}
+
+	config := Config{
+		DataDir:     appdirs.Data,
+		CookiePath:  filepath.Join(appdirs.Data, "MAM.cookie"),
+		IpPath:      filepath.Join(appdirs.Data, "MAM.ip"),
+		LastRunPath: filepath.Join(appdirs.Data, "last_run_time"),
+		Logger:      logger,
+	}
+
+	updater, err := NewMamUpdater(config)
+	if err != nil {
+		logger.Error("Failed to create updater", "error", err)
+		os.Exit(1)
+	}
+
+	if err := updater.Run(); err != nil {
+		logger.Error("Failed to run updater", "error", err)
+		os.Exit(1)
+	}
 }
