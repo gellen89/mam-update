9e358c731f7276bb38207be04bd590061e4951d0
gellen89 <186510403+gellen89@users.noreply.github.com>
2024-10-28 08:49:05 -0700

    simplifies appdir, adds flags

--- internal/appdir/appdir.go
+++ internal/appdir/appdir.go
@@ -12,28 +12,24 @@ type AppDirs struct {
 	Cache  string
 }
 
-func GetAppDirs(appName string) (AppDirs, error) {
-	userProvidedDir := os.Getenv("MAMUPDATE_DIR")
-	if userProvidedDir != "" {
-		return AppDirs{
-			Config: userProvidedDir,
-			Data:   userProvidedDir,
-			Cache:  filepath.Join(userProvidedDir, ".cache"),
-		}, nil
-	}
-
-	homeDir, err := os.UserHomeDir()
-	if err != nil {
-		return AppDirs{}, err
-	}
-
-	appDir := filepath.Join(homeDir, appName)
-
-	return AppDirs{
+// Returns app dirs with the given app dir
+func New(appDir string) *AppDirs {
+	return &AppDirs{
 		Config: appDir,
 		Data:   appDir,
 		Cache:  filepath.Join(appDir, ".cache"),
-	}, nil
+	}
+}
+
+// Returns app dirs at the user's homedir + the appName as a subdirectory
+func NewFromAppName(appName string) (*AppDirs, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return nil, err
+	}
+
+	appDir := filepath.Join(homeDir, appName)
+	return New(appDir), nil
 }
 
 // EnsureDirs creates all directories in the AppDirs struct if they don't exist
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"log/slog"
 	"os"
 	"path/filepath"
@@ -12,21 +13,25 @@ import (
 func main() {
 	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
 
-	appdirs, err := appdir.GetAppDirs(".mamupdate")
-	if err != nil {
-		logger.Error("Failed to get app directories", "error", err)
-		os.Exit(1)
+	flagCfg := getFlags()
+
+	mamId := getMamId(flagCfg)
+
+	var appdirs *appdir.AppDirs
+	if flagCfg.ConfigDir != nil && *flagCfg.ConfigDir != "" {
+		appdirs = appdir.New(*flagCfg.ConfigDir)
+	} else {
+		var err error
+		appdirs, err = appdir.NewFromAppName(".mamupdate")
+		if err != nil {
+			logger.Error("Failed to get app directories", "error", err)
+			os.Exit(1)
+		}
 	}
 
 	if err := appdirs.EnsureDirs(); err != nil {
 		logger.Error("Failed to ensure directories exist", "error", err)
 		os.Exit(1)
-	}
-
-	envMamId := os.Getenv("MAM_ID")
-	var mamId *string
-	if envMamId != "" {
-		mamId = &envMamId
 	}
 
 	config := mamupdater.Config{
@@ -48,4 +53,34 @@ func main() {
 		logger.Error("Failed to run updater", "error", err)
 		os.Exit(1)
 	}
+}
+
+type flagConfig struct {
+	MamId     *string
+	ConfigDir *string
+}
+
+func getFlags() *flagConfig {
+	var mamId string
+	flag.StringVar(&mamId, "mam-id", "", "Provide the mam-id used for the initial request.")
+	var mamDir string
+	flag.StringVar(&mamDir, "mam-dir", "", "Provide the directory where the config and data will be persisted.")
+
+	flag.Parse()
+
+	return &flagConfig{
+		MamId:     &mamId,
+		ConfigDir: &mamDir,
+	}
+}
+
+func getMamId(flagCfg *flagConfig) *string {
+	if flagCfg.MamId != nil && *flagCfg.MamId != "" {
+		return flagCfg.MamId
+	}
+	envMamId := os.Getenv("MAM_ID")
+	if envMamId != "" {
+		return &envMamId
+	}
+	return nil
 }
