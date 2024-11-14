420a087da0a707a86b8e7323c7433b147352ae65
gellen89 <186510403+gellen89@users.noreply.github.com>
2024-11-14 15:12:38 -0800

    updates logger to be json

--- main.go
+++ main.go
@@ -12,7 +12,7 @@ import (
 )
 
 func main() {
-	logger := slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{}))
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{}))
 
 	flagCfg := getFlags()
 
